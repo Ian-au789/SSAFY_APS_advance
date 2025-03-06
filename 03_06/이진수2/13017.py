2037e6c03ca5060b3465ad7a77818165c8503d6d
Ian <ijae0611@gmail.com>
2025-03-06 10:30:28 +0900

    decimal

---
+++ 03_06/이진수2/13017.py
@@ -0,0 +1,28 @@
+# https://swexpertacademy.com/main/talk/solvingClub/problemView.do?solveclubId=AZTP1QzqXnbHBIRD&contestProbId=AXwz6r6qAKUDFASZ&probBoxId=AZVkSAOaDPrHBINE&type=USER&problemBoxTitle=%2803.06%29+start+1&problemBoxCnt=4
+
+import sys
+sys.stdin = open("sample_input(18).txt")
+
+
+T = int(input())
+for t in range(1, T+1):
+    N = float(input())
+
+    result = ""
+    n = -1
+
+    while N > 0:
+        if N >= 2 ** n:
+            N -= 2 ** n
+            result += "1"
+
+        else:
+            result += "0"
+
+        n -= 1
+
+        if len(result) > 13:
+            result = "overflow"
+            break
+
+    print(f"#{t} {result}")
