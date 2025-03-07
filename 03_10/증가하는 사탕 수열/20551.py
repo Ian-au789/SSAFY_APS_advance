3d0a534488370a575338f0f679c88dd43583e73c
Ian <ijae0611@gmail.com>
2025-03-07 12:52:29 +0900

    next week

---
+++ 03_10/증가하는 사탕 수열/20551.py
@@ -0,0 +1,1 @@
+# https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AY4XhKTKU0IDFARM
---
+++ 03_10/홈 방범 서비스/2117.py
@@ -0,0 +1,51 @@
+import sys
+sys.stdin = open("sample_input.txt")
+
+
+def security_service(ci, cj, size):
+    global max_h
+    global houses
+    global M
+    global N
+
+    cost = size ** 2 + (size - 1) ** 2
+    cnt = 0
+
+    # 탐색 범위가 최댓값보다 낮으면 탐색 중단
+    if max_h > cost:
+        return
+
+    # 범위가 너무 커서 모든 집을 서비스해도 손해가 발생하면 탐색 불가
+    if cost > houses * M:
+        return
+
+    for di in range(-(size - 1), size):
+        for dj in range(-(size - 1 - abs(di)), (size - 1 - abs(di)) + 1):
+            ni = ci + di
+            nj = cj + dj
+            if 0 <= ni < N and 0 <= nj < N:
+                if matrix[ni][nj] == 1:
+                    cnt += 1
+
+    if cnt * M >= cost:
+        max_h = max(max_h, cnt)
+
+    return
+
+
+T = int(input())
+for t in range(1, T+1):
+    N, M = map(int, input().split())
+    matrix = [list(map(int, input().split())) for _ in range(N)]
+    max_h = 0
+
+    houses = 0
+    for row in matrix:
+        houses += sum(row)
+
+    for i in range(N):
+        for j in range(N):
+            for k in range(N+1, 0, -1):
+                security_service(i, j, k)
+
+    print(f"#{t} {max_h}")
