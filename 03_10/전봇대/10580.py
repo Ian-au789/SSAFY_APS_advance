5cf7aebef6fa2d6ffa66ffa7234d49967ad1ff32
Ian <ijae0611@gmail.com>
2025-03-07 14:33:06 +0900

    ready

--- 03_10/전봇대/10580.py
+++ 03_10/전봇대/10580.py
@@ -1,2 +1,31 @@
 # https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AXO8QBw6Qu4DFAXS
 
+import sys
+sys.stdin = open("input.txt")
+
+
+'''
+1. 교차점 발생 조건 (시작점이 Ai보다 높으면 도착점은 Bi보다 낮아야 하고 시작점이 Ai보다 낮으면 도착점은 Bi보다 높아야 한다.)
+2. 교차점을 중복 탐색하지 않게 이미 탐색한 전봇대는 또 탐색 대상에 넣지 않는다
+'''
+
+T = int(input())
+for t in range(1, T+1):
+    N = int(input())
+    jacks = [list(map(int, input().split())) for _ in range(N)]
+
+    cnt = 0
+
+    for i in range(N - 1):
+        start = jacks[i][0]
+        end = jacks[i][1]
+        for j in range(i + 1, N):
+            if jacks[j][0] > start:
+                if jacks[j][1] < end:
+                    cnt += 1
+
+            else:
+                if jacks[j][1] > end:
+                    cnt += 1
+
+    print(f"#{t} {cnt}")
--- 03_10/증가하는 사탕 수열/20551.py
+++ 03_10/증가하는 사탕 수열/20551.py
@@ -1,1 +1,36 @@
 # https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AY4XhKTKU0IDFARM
+
+import sys
+sys.stdin = open("sample_input.txt")
+
+'''
+1. 세 번째 상자가 2 이하의 사탕을 가지면 불가능
+2. 2번째 상자가 3번째 상자보다 1개 적도록 사탕을 먹는다. 그 다음도 동일
+'''
+
+
+def candy(a, b, c):
+    if c < 3 or b == 1:
+        return -1
+
+    elif a < b < c:
+        return 0
+
+    else:
+        eat = 0
+
+        if b >= c:
+            eat += b - c + 1
+            b = c - 1
+
+        if a >= b:
+            eat += a - b + 1
+
+    return eat
+
+
+T = int(input())
+for t in range(1, T+1):
+    A, B, C = map(int, input().split())
+
+    print(f"#{t} {candy(A, B, C)}")
