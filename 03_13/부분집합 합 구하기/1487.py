4ce0baeaea316d1bad92558de16f6edaf26484c3
Ian <ijae0611@gmail.com>
2025-03-13 10:31:19 +0900

    sum subset

--- 03_13/부분집합 합 구하기/1487.py
+++ 03_13/부분집합 합 구하기/1487.py
@@ -5,15 +5,15 @@ sys.stdin = open("sample_input.txt")
 def dfs(subset, size, num, idx):
     global cnt
 
-    if subset == num:
+    if subset == num:        # 원하는 부분합에 도달하면 카운트
         cnt += 1
         return
 
-    elif subset > num or idx == size:
+    elif subset > num or idx == size:        # 원하는 부분합을 넘거나 모든 원소를 탐색했으면 중지
         return
 
     else:
-        dfs(subset + elements[idx], size, num, idx + 1,)
+        dfs(subset + elements[idx], size, num, idx + 1,)     # 해당 원소를 더할까 말까
         dfs(subset, size, num, idx + 1)
 
 
--- 03_13/부분집합 합 구하기/1487_DP.py
+++ 03_13/부분집합 합 구하기/1487_DP.py
@@ -6,8 +6,12 @@ T = int(input())
 for t in range(1, T+1):
     N, W = map(int, input().split())
     elements = list(map(int, input().split()))
-    dp = [-1] * (1 << N)
 
+    dp = [0] * (W+1)       # 부분 집합의 합을 인덱스로 가지는 배열
+    dp[0] = 1              # 공집합 가능한 경우 1개
 
+    for e in elements:
+        for i in range(W, e-1, -1):       # 중복 방지를 위해 뒤에서부터 탐색
+            dp[i] += dp[i - e]
 
-    print(f"#{t} {}")
+    print(f"#{t} {dp[W]}")
