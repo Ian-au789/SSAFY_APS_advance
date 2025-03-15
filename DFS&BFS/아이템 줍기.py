454d0807c78b9323a85c787a838e0d27690102ce
Ian <ijae0611@gmail.com>
2025-03-15 20:42:57 +0900

    item

---
+++ DFS&BFS/아이템 줍기.py
@@ -0,0 +1,5 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/87694?language=python3
+
+def solution(rectangle, characterX, characterY, itemX, itemY):
+    answer = 0
+    return answer
