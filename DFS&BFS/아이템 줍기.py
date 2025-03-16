478e61a4bbf44f0f78c08f57f61afcdb85d33144
Ian <ijae0611@gmail.com>
2025-03-16 22:59:44 +0900

    done

--- DFS&BFS/아이템 줍기.py
+++ DFS&BFS/아이템 줍기.py
@@ -1,5 +1,8 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/87694?language=python3
 
+'''
+1. 겹쳐진 상자가 바뀔 떄가 포인트
+'''
 def solution(rectangle, characterX, characterY, itemX, itemY):
     answer = 0
     return answer
