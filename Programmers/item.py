a6386e33046ce20d3b0e17f36ec8930c12077e67
Ian <ijae0611@gmail.com>
2025-03-19 23:35:43 +0900

    double up

--- Programmers/item.py
+++ Programmers/item.py
@@ -1,59 +1,49 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/87694
 
 '''
-1. BFS를 써서 목표 지점까지 최단경로 확인
-2. 델타 탐색으로 다음 좌표가 사각형의 테두리에서만 움직이도록 함
-3. 해당 좌표가 속하는 사각형 테두리는 전부 탐색
-4. 만약 상자 내부에 들어가거나, 직사각형 한 변의 길이가 2라서 관통하지 않도록 제한
+기존 좌표계에서는 테두리가 사실상 한 넓이를 차지하기 때문에
+직사각형이 바로 인접해있거나 가로 세로 길이가 2인 직사각형은 내부 공간과 테두리를 구분하기 어렵다.
+
+그냥 좌표계를 2배로 뻥튀기하면 무조건 공백이 생긴다. 만사 해결
 '''
 
 from collections import deque
 
-
 def solution(rectangle, characterX, characterY, itemX, itemY):
+    board = [[0] * 102 for _ in range(102)]
+    visited = [[0] * 102 for _ in range(102)]
     queue = deque()
-    queue.append([(characterX, characterY), 0])
-    visited = [[0]*51 for _ in range(51)]
+
+    # 좌표계 2배 확장해서  사용
+    characterX, characterY, itemX, itemY = characterX * 2, characterY * 2, itemX * 2, itemY * 2
+    for x1, y1, x2, y2 in rectangle:
+        x1, y1, x2, y2 = x1 * 2, y1 * 2, x2 * 2, y2 * 2
+        for i in range(x1, x2 + 1):
+            for j in range(y1, y2 + 1):
+                if x1 < i < x2 and y1 < j < y2:
+                    board[i][j] = 2     # 내부 영역 (이동 불가능)
+                elif board[i][j] != 2:
+                    board[i][j] = 1      # 테두리 (이동 가능)
+
     di = [1, 0, -1, 0]
     dj = [0, 1, 0, -1]
 
-    while queue:
-        cur_loc, level = queue.popleft()
-        visited[cur_loc[0]][cur_loc[1]] = 1
+    queue.append((characterX, characterY, 0))
+    visited[characterX][characterY] = 1
 
-        if cur_loc == (itemX, itemY):
-            break
+    while queue:
+        ci, cj, dist = queue.popleft()
+
+        # 목표 위치 도달
+        if (ci, cj) == (itemX, itemY):
+            return dist // 2  # 원래 좌표 기준으로 복원
 
         for k in range(4):
-            next_loc = (cur_loc[0] + di[k], cur_loc[1] + dj[k])
+            ni, nj = ci + di[k], cj + dj[k]
 
-            if visited[next_loc[0]][next_loc[1]]:
-                continue
-
-            cur_box = []
-
-            # 현재 테두리 위에 있는 직사각형 저장
-            for rect in rectangle:
-                if next_loc[0] in [rect[0], rect[2]] and next_loc[1] in range(rect[1], rect[3] + 1) or\
-                    next_loc[0] in range(rect[0], rect[2] + 1) and next_loc[1] in [rect[1], rect[3]]:
-                    cur_box.append(rect)
-                else:
-                    continue
-
-            if len(cur_box) == 0:
-                continue
-
-            # 그 외에 다른 상자 내부에 있나 확인
-            for r in rectangle:
-                if r not in cur_box:
-                    if next_loc[0] in range(r[0] + 1, r[2]) or next_loc[1] in range(r[1] + 1, r[3]):
-                        break
-                    else:
-                        continue
-            else:
-                queue.append([next_loc, level + 1])
-
-    return level
+            if not visited[ni][nj] and board[ni][nj] == 1:
+                visited[ni][nj] = 1
+                queue.append((ni, nj, dist + 1))
 
 
 print(solution([[1, 1, 7, 4], [3, 2, 5, 5], [4, 3, 6, 9], [2, 6, 8, 8]], 1, 3, 7, 8))     # 17
@@ -61,3 +51,4 @@ print(solution([[1, 1, 8, 4], [2, 2, 4, 9], [3, 6, 9, 8], [6, 3, 7, 7]], 9, 7, 6
 print(solution([[1, 1, 5, 7]], 1, 1, 4, 7))     # 9
 print(solution([[2, 1, 7, 5], [6, 4, 10, 10]], 3, 1, 7, 10))     # 15
 print(solution([[2, 2, 5, 5], [1, 3, 6, 4], [3, 1, 4, 6]], 1, 4, 6, 3))     # 10
+
--- 연습장.py
+++ 연습장.py
@@ -1,47 +1,22 @@
-from collections import deque
+# for t in range(1, T+1):
+#     N = int(input())
+#     matrix = [list(map(int, input().split())) for _ in range(N)]
 
-def solution(rectangle, characterX, characterY, itemX, itemY):
-    board = [[0] * 102 for _ in range(102)]
-    visited = [[0] * 102 for _ in range(102)]
-    queue = deque()
+from collections import defaultdict
 
-    # 좌표를 2배 확장하여 사용
-    characterX, characterY, itemX, itemY = characterX * 2, characterY * 2, itemX * 2, itemY * 2
-    for x1, y1, x2, y2 in rectangle:
-        x1, y1, x2, y2 = x1 * 2, y1 * 2, x2 * 2, y2 * 2
-        for i in range(x1, x2 + 1):
-            for j in range(y1, y2 + 1):
-                if x1 < i < x2 and y1 < j < y2:
-                    board[i][j] = -1     # 내부 영역 (이동 불가능)
-                elif board[i][j] != -1:
-                    board[i][j] = 1      # 테두리 (이동 가능)
+word_count = defaultdict(int)
+word_count['apple'] += 1
+print(word_count['apple'])  # 출력: 1
 
-    dx = [1, 0, -1, 0]
-    dy = [0, 1, 0, -1]
+list_dict = defaultdict(list)
+list_dict['fruits'].append('apple')
+list_dict['fruits'].append('banana')
+print(list_dict['fruits'])  # 출력: ['apple', 'banana']
 
-    queue.append((characterX, characterY, 0))
-    visited[characterX][characterY] = 1
-
-    while queue:
-        x, y, dist = queue.popleft()
-
-        # 목표 위치 도달
-        if (x, y) == (itemX, itemY):
-            return dist // 2  # 원래 좌표 기준으로 복원
-
-        for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
-
-            if not visited[nx][ny] and board[nx][ny] == 1:
-                visited[nx][ny] = 1
-                queue.append((nx, ny, dist + 1))
-
-
-print(solution([[1, 1, 7, 4], [3, 2, 5, 5], [4, 3, 6, 9], [2, 6, 8, 8]], 1, 3, 7, 8))     # 17
-print(solution([[1, 1, 8, 4], [2, 2, 4, 9], [3, 6, 9, 8], [6, 3, 7, 7]], 9, 7, 6, 1))     # 11
-print(solution([[1, 1, 5, 7]], 1, 1, 4, 7))     # 9
-print(solution([[2, 1, 7, 5], [6, 4, 10, 10]], 3, 1, 7, 10))     # 15
-print(solution([[2, 2, 5, 5], [1, 3, 6, 4], [3, 1, 4, 6]], 1, 4, 6, 3))     # 10
-
+set_dict = defaultdict(set)
+set_dict['fruits'].add('apple')
+set_dict['fruits'].add('banana')
+set_dict['fruits'].add('apple')
+print(set_dict['fruits'])  # 출력: {'apple', 'banana'}
 
 
