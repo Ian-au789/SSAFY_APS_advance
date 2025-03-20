12a88cf0e068a4342e96c1949c1fe2a5f5cdd5d2
Ian <ijae0611@gmail.com>
2025-03-20 11:13:36 +0900

    dijkstra

--- 연습장.py
+++ 연습장.py
@@ -20,44 +20,39 @@ set_dict['fruits'].add('apple')
 print(set_dict['fruits'])  # 출력: {'apple', 'banana'}
 
 
-def find_set(x):   # 부모 노드 탐색 & 경로 압축
-    if x != p[x]:
-        p[x] = find_set(p[x])
-    return p[x]
+import heapq
 
 
-def union(x, y):   # 경로 연결 & 사이클 방지
-    px = find_set(x)
-    py = find_set(y)
+def dijkstra(start_node):
+    pq = [(0, start_node)]
+    dists = [MAX] * V
+    dists[start_node] = 0
 
-    if px != py:
-        if px < py:
-            p[py] = px
-        else:
-            p[px] = py
-    else:
-        return
+    while pq:
+        dist, node = heapq.heappop(pq)
 
+        if dists[node] < dist:      # 이미 더 작은 경로가 존재한다면 pass
+            continue
+
+        for next_info in graph[node]:
+            next_dist = next_info[0]
+            next_node = next_info[1]
+
+            new_dist = dist + next_dist
+
+            if dists[next_node] <= new_dist:
+                continue
+            else:
+                dists[next_node] = new_dist
+                heapq.heappush(pq, (new_dist, next_node))
+
+
+MAX = 1e9
 
 V, E = map(int, input().split())
-edges = []
+start_node = 0
+graph = [[] for _ in range(V)]
 
 for _ in range(E):
-    start, end, weight = map(int, input().split())
-    edges.append((start, end, weight))
-
-edges.sort(key=lambda x: x[2])           # 가중치 기준으로 오름차순 정렬
-p = [i for i in range(V)]          # 부모 노드 (make_set)
-
-cnt = 0             # 선택한 간선 개수
-result = 0          # 가중치의 합
-
-for u, v, w in edges:
-
-    if find_set(u) != find_set(v):    # u와 v가 연결이 안 되어있다면
-        union(u, v)
-        cnt += 1
-        result += w
-
-        if cnt == V - 1:
-            break
+    u, v, w = map(int, input().split())
+    graph[u].append((w, v))           # 단방향 그래프
