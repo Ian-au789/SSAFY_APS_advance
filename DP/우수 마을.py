0ee3fe9e481cd6a3f0062a8dc77892fc53c13217
Ian <ijae0611@gmail.com>
2025-03-21 17:03:39 +0900

    tree DP

--- DP/우수 마을.py
+++ DP/우수 마을.py
@@ -1,9 +1,16 @@
 # https://www.acmicpc.net/problem/1949
+from collections import defaultdict
 
-
-def
+def best_village():
+    pass
 
 
 N = 7
 population = [1000, 3000, 4000, 1000, 2000, 2000, 7000]
 edges = [(1, 2), (2, 3), (4, 3), (4, 5), (6, 2), (6, 7)]
+graph = defaultdict(set)
+for e in edges:
+    graph[e[0]].add(e[1])
+    graph[e[1]].add(e[0])
+
+print(graph)
--- 연습장.py
+++ 연습장.py
@@ -18,25 +18,3 @@ set_dict['fruits'].add('apple')
 set_dict['fruits'].add('banana')
 set_dict['fruits'].add('apple')
 print(set_dict['fruits'])  # 출력: {'apple', 'banana'}
-
-
-def floyd_warshall(graph):
-    v_len = len(graph)     # 그래프의 정점 수
-    dist = [[1e9] * v_len for _ in range(v_len)]       # 거리 행렬
-
-    for i in range(v_len):
-        dist[i][i] = 0              # 자기 자신으로의 거리는 0
-
-    for u in range(v_len):
-        for v in range(v_len):
-            if graph[u][v] != 0:
-                dist[u][v] = graph[u][v]    # 인접 행렬사이의 거리로 업데이트
-
-    # 가능한 모든 시작점, 경유 루트, 도착점의 경우를 탐색하면서 최단 경로 갱신
-    for k in range(v_len):
-        for i in range(v_len):
-            for j in range(v_len):
-                if dist[i][k] + dist[k][j] < dist[i][j]:
-                    dist[i][j] = dist[i][k] + dist[k][j]
-
-    return dist      # 모든 정점 사이의 최단거리가 반영된 행렬
